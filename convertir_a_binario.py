09f688a41ea8417a2db928e1ea027608a8fde84e
jzmx <jorge@zaldivar.mx>
2023-10-19 02:04:30 +0000

    Code solution

--- convertir_a_binario.py
+++ convertir_a_binario.py
@@ -1,15 +1,21 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Convertir a binario
 """
 
-# Declaraciones
-CONSTANTE = valor
-
 # Entradas
-entrada = input()
+numero = int(input("Introduzca un número: "))
 
 # Proceso
+dividendo = numero
+binario = ""
+while dividendo > 0:
+    residuo = dividendo % 2
+    dividendo //= 2
+    binario = str(residuo) + binario
 
+# Completar a múltiplo de ocho dígitos
+if len(binario) % 8 != 0:
+    binario = "0" * (8 - len(binario) % 8) + binario
 
 # Salidas
-print(salida)
+print(numero, binario)
